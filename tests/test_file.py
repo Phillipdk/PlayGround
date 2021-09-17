876dd144a9dd3aafcea910d4e905c1584acdef50
Phillipdk <72807319+PhillipEDRDK@users.noreply.github.com>
2021-09-17 08:41:37 +0200

    Create test_file.py

---
+++ tests/test_file.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 17 08:32:58 2021
+
+@author: 1990phkj
+"""
+
+import os
+
+def test_simple():
+    assert True
+    
+def test_get_github_secrets():
+    s = os.environ.get("Simple")
+    assert s == "Simple"
