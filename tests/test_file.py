047516b3889943389e2a71667cc40cda8a4a33e9
Phillipdk <72807319+PhillipEDRDK@users.noreply.github.com>
2021-09-29 09:32:46 +0200

    Added a tutorial and a test for it

--- tests/test_file.py
+++ tests/test_file.py
@@ -6,6 +6,7 @@ Created on Fri Sep 17 08:32:58 2021
 """
 
 import os
+import runpy
 
 def test_simple():
     assert True
@@ -13,3 +14,6 @@ def test_simple():
 def test_get_github_secrets():
     s = os.environ.get("SIMPLE")
     assert s == "Simple"
+    
+def run_tutorial():
+    runpy.run_path("C:\\Users\\1990phkj\\Documents\\IoT_Suite\\PlayGround\\Tutorial.py")
